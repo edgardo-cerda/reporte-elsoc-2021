ea6d3cbb956b5b541744aadb33953471e8912354
edgardo-cerda <edgardo.cerda.e@gmail.com>
2021-09-23 15:55:52 -0300

    Create chile_sin_islas.R

---
+++ 1_input/chile_sin_islas.R
@@ -0,0 +1,17 @@
+library(chilemapas)
+library(tidyverse)
+library(sf)
+
+#################################################################
+########### Mapas de Chile para analisis geográfico #############
+#################################################################
+
+### Mapa de chile en tsmap a partir de 
+mapa_chile_comunas <- chilemapas::mapa_comunas %>% 
+  dplyr::filter(!codigo_comuna %in% c('05201', '05104')) %>% # Sacar Rapa Nui e Isla Juan Fernández
+  st_as_sf(.)
+
+# Borrar islas de la comuna de valparaiso
+mapa_chile_comunas[[4]][[76]][[4]] <- NULL
+mapa_chile_comunas[[4]][[76]][[3]] <- NULL
+mapa_chile_comunas[[4]][[76]][[2]] <- NULL
